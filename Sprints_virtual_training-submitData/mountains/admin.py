08e0a7cfd42d562112507bded528bd6de8a3ba3d
kingtvs12 <122884145+kingtvs12@users.noreply.github.com>
2023-07-27 19:13:19 +0300

    Add files via upload

---
+++ Sprints_virtual_training-submitData/mountains/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Pass
+
+admin.site.register(Pass)
---
+++ Sprints_virtual_training-submitData/sprints/urls.py
@@ -0,0 +1,43 @@
+"""
+URL configuration for sprints project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.views.generic import TemplateView
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Mountains API",
+        default_version="v1",
+        description="This API provides access to data and services for our project",
+        terms_of_service="",  # Здесь может быть ссылка на условия использования (пустая строка в данном случае)
+        contact=openapi.Contact(email="contact@example.com"),  # Здесь указывается контактный email
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('swagger-ui/', TemplateView.as_view(template_name='swagger-ui.html'), name='swagger-ui'),
+    path('api/', include('mountains.urls')),
+]
+
